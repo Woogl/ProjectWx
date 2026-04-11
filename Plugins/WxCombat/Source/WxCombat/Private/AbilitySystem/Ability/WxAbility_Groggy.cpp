7a982a03ea76aea6df225665f2e4c62e79a74a9d
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-04-11 23:56:25 +0900

    Groggy 어빌리티 리팩토링

--- Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Ability/WxAbility_Groggy.cpp
+++ Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Ability/WxAbility_Groggy.cpp
@@ -4,7 +4,9 @@
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/WxCombatAttributeSet.h"
 #include "AbilitySystem/Effect/WxEffect_DrainDP.h"
-#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Animation/AnimInstance.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
 #include "WxGameplayTags.h"
 
 UWxAbility_Groggy::UWxAbility_Groggy()
@@ -38,71 +40,57 @@ void UWxAbility_Groggy::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		return;
 	}
 
-	// PP를 0으로 설정
 	ASC->SetNumericAttributeBase(UWxCombatAttributeSet::GetPPAttribute(), 0.f);
 
-	// State.Groggy 태그 제거 감지 → 어빌리티 종료
 	GroggyTagDelegateHandle = ASC->RegisterGameplayTagEvent(WxGameplayTags::State_Groggy, EGameplayTagEventType::NewOrRemoved)
 		.AddUObject(this, &UWxAbility_Groggy::HandleGroggyTagChanged);
 
-	// 다른 몽타주가 재생 중이면 그 몽타주가 끝날 때까지 대기한 뒤 그로기 몽타주를 재생한다.
-	if (UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance())
-	{
-		if (AnimInstance->GetCurrentActiveMontage() != nullptr)
-		{
-			AnimInstance->OnMontageEnded.AddDynamic(this, &UWxAbility_Groggy::HandleMontageEnded);
-		}
-		else
-		{
-			PlayGroggyMontage();
-		}
-	}
-	else
-	{
-		PlayGroggyMontage();
-	}
-
-	// DP 드레인 이펙트 적용
 	FGameplayEffectSpecHandle DrainSpecHandle = MakeOutgoingGameplayEffectSpec(UWxEffect_DrainDP::StaticClass(), GetAbilityLevel());
 	if (DrainSpecHandle.IsValid())
 	{
 		DrainDPEffectHandle = ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, DrainSpecHandle);
 	}
+
+	if (UWorld* World = ActorInfo->AvatarActor.IsValid() ? ActorInfo->AvatarActor->GetWorld() : nullptr)
+	{
+		World->GetTimerManager().SetTimer(MontagePollingTimerHandle, this, &UWxAbility_Groggy::TickPlayMontage, 0.1f, true);
+	}
+
+	TickPlayMontage();
 }
 
 void UWxAbility_Groggy::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	// PP를 MaxPP로 회복
-	if (ActorInfo && ActorInfo->AbilitySystemComponent.IsValid())
-	{
-		UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
-		if (const UWxCombatAttributeSet* AttributeSet = ASC->GetSet<UWxCombatAttributeSet>())
-		{
-			ASC->SetNumericAttributeBase(UWxCombatAttributeSet::GetPPAttribute(), AttributeSet->GetMaxPP());
-		}
-
-		// DP 드레인 이펙트 제거
-		if (DrainDPEffectHandle.IsValid())
-		{
-			ASC->RemoveActiveGameplayEffect(DrainDPEffectHandle);
-			DrainDPEffectHandle.Invalidate();
-		}
-	}
-
-	// OnMontageEnded 델리게이트 정리
 	if (ActorInfo)
 	{
-		if (UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance())
+		if (UWorld* World = ActorInfo->AvatarActor.IsValid() ? ActorInfo->AvatarActor->GetWorld() : nullptr)
 		{
-			AnimInstance->OnMontageEnded.RemoveDynamic(this, &UWxAbility_Groggy::HandleMontageEnded);
+			World->GetTimerManager().ClearTimer(MontagePollingTimerHandle);
 		}
-	}
 
-	if (GroggyTagDelegateHandle.IsValid() && ActorInfo && ActorInfo->AbilitySystemComponent.IsValid())
-	{
-		ActorInfo->AbilitySystemComponent->RegisterGameplayTagEvent(WxGameplayTags::State_Groggy, EGameplayTagEventType::NewOrRemoved)
-			.Remove(GroggyTagDelegateHandle);
-		GroggyTagDelegateHandle.Reset();
+		if (ActorInfo->AbilitySystemComponent.IsValid())
+		{
+			UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+			if (const UWxCombatAttributeSet* AttributeSet = ASC->GetSet<UWxCombatAttributeSet>())
+			{
+				ASC->SetNumericAttributeBase(UWxCombatAttributeSet::GetPPAttribute(), AttributeSet->GetMaxPP());
+			}
+
+			ASC->StopMontageIfCurrent(*GroggyMontage);
+
+			if (DrainDPEffectHandle.IsValid())
+			{
+				ASC->RemoveActiveGameplayEffect(DrainDPEffectHandle);
+				DrainDPEffectHandle.Invalidate();
+			}
+
+			if (GroggyTagDelegateHandle.IsValid())
+			{
+				ASC->RegisterGameplayTagEvent(WxGameplayTags::State_Groggy, EGameplayTagEventType::NewOrRemoved)
+					.Remove(GroggyTagDelegateHandle);
+				GroggyTagDelegateHandle.Reset();
+			}
+		}
 	}
 
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
@@ -116,75 +104,24 @@ void UWxAbility_Groggy::HandleGroggyTagChanged(const FGameplayTag CallbackTag, i
 	}
 }
 
-void UWxAbility_Groggy::HandleMontageCompleted()
+void UWxAbility_Groggy::TickPlayMontage()
 {
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-}
-
-void UWxAbility_Groggy::HandleMontageBlendOut()
-{
-	// OnCompleted가 후속 발동하므로 여기서는 처리하지 않음
-}
-
-void UWxAbility_Groggy::HandleMontageInterrupted()
-{
-	// 다른 몽타주(HitReact 등)에 의해 중단된 경우, 해당 몽타주 종료 시 그로기 몽타주를 재재생한다.
-	if (CurrentActorInfo && CurrentActorInfo->AbilitySystemComponent.IsValid()
-		&& CurrentActorInfo->AbilitySystemComponent->HasMatchingGameplayTag(WxGameplayTags::State_Groggy))
-	{
-		if (UAnimInstance* AnimInstance = CurrentActorInfo->GetAnimInstance())
-		{
-			AnimInstance->OnMontageEnded.AddDynamic(this, &UWxAbility_Groggy::HandleMontageEnded);
-		}
-	}
-}
-
-void UWxAbility_Groggy::HandleMontageCancelled()
-{
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
-}
-
-void UWxAbility_Groggy::HandleMontageEnded(UAnimMontage* Montage, bool bInterrupted)
-{
-	// 그로기 자신의 몽타주 blend out 완료는 무시한다.
-	if (Montage == GroggyMontage)
+	if (!CurrentActorInfo)
 	{
 		return;
 	}
 
-	// 다른 몽타주에 의해 interrupt된 경우, 새 몽타주가 끝날 때까지 계속 대기한다.
-	if (bInterrupted)
+	const UAnimInstance* AnimInstance = CurrentActorInfo->GetAnimInstance();
+	if (!AnimInstance || AnimInstance->GetCurrentActiveMontage() != nullptr)
 	{
-		if (UAnimInstance* AnimInstance = CurrentActorInfo->GetAnimInstance())
-		{
-			if (AnimInstance->GetCurrentActiveMontage() != nullptr)
-			{
-				return;
-			}
-		}
-	}
-
-	if (UAnimInstance* AnimInstance = CurrentActorInfo->GetAnimInstance())
-	{
-		AnimInstance->OnMontageEnded.RemoveDynamic(this, &UWxAbility_Groggy::HandleMontageEnded);
-	}
-
-	PlayGroggyMontage();
-}
-
-void UWxAbility_Groggy::PlayGroggyMontage()
-{
-	UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-		this, NAME_None, GroggyMontage, 1.f, NAME_None, true, 1.f, 0.f, true);
-	if (!MontageTask)
-	{
-		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 		return;
 	}
 
-	MontageTask->OnCompleted.AddDynamic(this, &UWxAbility_Groggy::HandleMontageCompleted);
-	MontageTask->OnBlendOut.AddDynamic(this, &UWxAbility_Groggy::HandleMontageBlendOut);
-	MontageTask->OnInterrupted.AddDynamic(this, &UWxAbility_Groggy::HandleMontageInterrupted);
-	MontageTask->OnCancelled.AddDynamic(this, &UWxAbility_Groggy::HandleMontageCancelled);
-	MontageTask->ReadyForActivation();
+	UAbilitySystemComponent* ASC = CurrentActorInfo->AbilitySystemComponent.Get();
+	if (!ASC)
+	{
+		return;
+	}
+
+	ASC->PlayMontage(this, CurrentActivationInfo, GroggyMontage, 1.f, NAME_None, 0.f);
 }
--- Plugins/WxCombat/Source/WxCombat/Public/AbilitySystem/Ability/WxAbility_Groggy.h
+++ Plugins/WxCombat/Source/WxCombat/Public/AbilitySystem/Ability/WxAbility_Groggy.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "AbilitySystem/Ability/WxAbilityBase.h"
 #include "ActiveGameplayEffectHandle.h"
+#include "Engine/EngineTypes.h"
 #include "WxAbility_Groggy.generated.h"
 
 class UAnimMontage;
@@ -13,9 +14,10 @@ class UAnimMontage;
  * 그로기 어빌리티.
  *
  * 사용 흐름:
- *  1. DP가 MaxDP에 도달 → State.Groggy 태그 부여 + Event.Groggy 이벤트 발송
- *  2. GameplayEvent 트리거 → ActivateAbility
- *  3. 몽타주 재생, State.Groggy 태그 제거 감지 시 몽타주 중단 → EndAbility
+ *  1. DP가 MaxDP에 도달 → State.Groggy 태그 부여
+ *  2. OwnedTagPresent 트리거로 ActivateAbility
+ *  3. 폴링으로 활성 몽타주가 없을 때 그로기 몽타주 재생, DP 드레인 적용
+ *  4. State.Groggy 태그 제거 감지 시 EndAbility
  *
  * 그로기 발동 중 HitReact를 제외한 모든 어빌리티 차단.
  * State.Dead 시 활성화 차단.
@@ -38,23 +40,9 @@ protected:
 private:
 	void HandleGroggyTagChanged(const FGameplayTag CallbackTag, int32 NewCount);
 
-	UFUNCTION()
-	void HandleMontageCompleted();
-
-	UFUNCTION()
-	void HandleMontageBlendOut();
-
-	UFUNCTION()
-	void HandleMontageInterrupted();
-
-	UFUNCTION()
-	void HandleMontageCancelled();
-
-	UFUNCTION()
-	void HandleMontageEnded(UAnimMontage* Montage, bool bInterrupted);
-
-	void PlayGroggyMontage();
+	void TickPlayMontage();
 
 	FDelegateHandle GroggyTagDelegateHandle;
 	FActiveGameplayEffectHandle DrainDPEffectHandle;
+	FTimerHandle MontagePollingTimerHandle;
 };
