0de9b3218426b67aa3a1b4204567e3829935ccdd
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-04-13 00:33:13 +0900

    BT ActivateAbility 태스크의 간헐적 정지 버그 수정

--- Source/WxGame/AI/WxBTTask_ActivateAbility.cpp
+++ Source/WxGame/AI/WxBTTask_ActivateAbility.cpp
@@ -47,8 +47,17 @@ EBTNodeResult::Type UWxBTTask_ActivateAbility::ExecuteTask(UBehaviorTreeComponen
 		return EBTNodeResult::Failed;
 	}
 
+	// TryActivateAbility 내부에서 어빌리티가 동기적으로 종료될 수 있다 (CommitAbility 실패 등).
+	// 이 경우 OnAbilityEnded가 이미 브로드캐스트된 후이므로, 델리게이트를 등록해도 콜백이 발생하지 않아
+	// BT가 InProgress 상태로 영구 정지한다.
+	if (!Spec->IsActive())
+	{
+		return EBTNodeResult::Failed;
+	}
+
 	CachedASC = ASC;
 	CachedOwnerComp = &OwnerComp;
+	ActivatedHandle = Spec->Handle;
 
 	AbilityEndedDelegateHandle = ASC->OnAbilityEnded.AddUObject(
 		this, &UWxBTTask_ActivateAbility::HandleAbilityEnded);
@@ -58,14 +67,23 @@ EBTNodeResult::Type UWxBTTask_ActivateAbility::ExecuteTask(UBehaviorTreeComponen
 
 EBTNodeResult::Type UWxBTTask_ActivateAbility::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// 델리게이트를 먼저 해제하여, CancelAbilities가 트리거하는 OnAbilityEnded 콜백이
+	// FinishLatentTask를 호출하지 않도록 한다.
 	CleanUp();
+
+	if (UAbilitySystemComponent* ASC = CachedASC.Get())
+	{
+		FGameplayTagContainer Tags;
+		Tags.AddTag(AbilityTag);
+		ASC->CancelAbilities(&Tags);
+	}
+
 	return EBTNodeResult::Aborted;
 }
 
 void UWxBTTask_ActivateAbility::HandleAbilityEnded(const FAbilityEndedData& AbilityEndedData)
 {
-	if (!AbilityEndedData.AbilityThatEnded ||
-		!AbilityEndedData.AbilityThatEnded->GetAssetTags().HasTag(AbilityTag))
+	if (AbilityEndedData.AbilitySpecHandle != ActivatedHandle)
 	{
 		return;
 	}
@@ -91,6 +109,7 @@ void UWxBTTask_ActivateAbility::CleanUp()
 		ASC->OnAbilityEnded.Remove(AbilityEndedDelegateHandle);
 	}
 	AbilityEndedDelegateHandle.Reset();
+	ActivatedHandle = FGameplayAbilitySpecHandle();
 }
 
 FString UWxBTTask_ActivateAbility::GetStaticDescription() const
