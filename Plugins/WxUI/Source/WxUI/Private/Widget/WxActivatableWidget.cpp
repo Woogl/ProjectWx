116ef86617b3254bfbd44356b3657aef12ef105e
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-04-18 08:50:03 +0900

    UWxActivatableWidget에 TimeDilation 속성을 추가

--- Plugins/WxUI/Source/WxUI/Private/Widget/WxActivatableWidget.cpp
+++ Plugins/WxUI/Source/WxUI/Private/Widget/WxActivatableWidget.cpp
@@ -2,6 +2,42 @@
 
 #include "Widget/WxActivatableWidget.h"
 
+#include "Kismet/GameplayStatics.h"
+
+static constexpr float MinTimeDilation = 0.001f;
+
+void UWxActivatableWidget::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+
+	const float EffectiveDilation = FMath::Max(TimeDilation, MinTimeDilation);
+	if (EffectiveDilation != 1.0f)
+	{
+		UWorld* World = GetWorld();
+		if (World && World->IsNetMode(NM_Standalone))
+		{
+			const float Current = UGameplayStatics::GetGlobalTimeDilation(World);
+			UGameplayStatics::SetGlobalTimeDilation(World, Current * EffectiveDilation);
+		}
+	}
+}
+
+void UWxActivatableWidget::NativeOnDeactivated()
+{
+	const float EffectiveDilation = FMath::Max(TimeDilation, MinTimeDilation);
+	if (EffectiveDilation != 1.0f)
+	{
+		UWorld* World = GetWorld();
+		if (World && World->IsNetMode(NM_Standalone))
+		{
+			const float Current = UGameplayStatics::GetGlobalTimeDilation(World);
+			UGameplayStatics::SetGlobalTimeDilation(World, Current / EffectiveDilation);
+		}
+	}
+
+	Super::NativeOnDeactivated();
+}
+
 TOptional<FUIInputConfig> UWxActivatableWidget::GetDesiredInputConfig() const
 {
 	switch (InputMode)
