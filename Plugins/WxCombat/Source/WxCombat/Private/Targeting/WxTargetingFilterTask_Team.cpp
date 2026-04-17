8d139d18f105ca68ce8cf7fa086a26fd2142f7ed
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-04-18 08:29:34 +0900

    TargetingFilterTask_Team 리네이밍

--- Plugins/WxCombat/Source/WxCombat/Private/Targeting/WxTargetingFilterTask_Team.cpp
+++ Plugins/WxCombat/Source/WxCombat/Private/Targeting/WxTargetingFilterTask_Team.cpp
@@ -1,10 +1,10 @@
 // Copyright Woogle. All Rights Reserved.
 
-#include "Targeting/WxTargetingFilterTask_TeamAttitude.h"
+#include "Targeting/WxTargetingFilterTask_Team.h"
 #include "GenericTeamAgentInterface.h"
 #include "Types/TargetingSystemTypes.h"
 
-bool UWxTargetingFilterTask_TeamAttitude::ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const
+bool UWxTargetingFilterTask_Team::ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const
 {
 	if (const FTargetingSourceContext* SourceContext = FTargetingSourceContext::Find(TargetingHandle))
 	{
