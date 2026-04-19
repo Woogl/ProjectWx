e668a70fe56468e9b6ed8a23664a4b72988cb9e0
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-04-19 18:13:03 +0900

    BuffATK 최대 1중첩으로 작동하게 수정

--- Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Effect/WxEffect_BuffATK.cpp
+++ Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Effect/WxEffect_BuffATK.cpp
@@ -9,6 +9,11 @@ UWxEffect_BuffATK::UWxEffect_BuffATK()
 	DurationPolicy = EGameplayEffectDurationType::HasDuration;
 	DurationMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(10.f));
 
+	// 중첩 불가: 대상당 1스택만 유지, 재적용 시 Duration 갱신
+	StackingType = EGameplayEffectStackingType::AggregateByTarget;
+	StackLimitCount = 1;
+	StackDurationRefreshPolicy = EGameplayEffectStackingDurationPolicy::RefreshOnSuccessfulApplication;
+
 	FGameplayModifierInfo Modifier;
 	Modifier.Attribute = UWxCombatAttributeSet::GetATKAttribute();
 	Modifier.ModifierOp = EGameplayModOp::MultiplyAdditive;
