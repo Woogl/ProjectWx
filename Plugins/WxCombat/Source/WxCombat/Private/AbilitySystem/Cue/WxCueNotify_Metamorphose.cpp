b3e8bef51c67c0522a652482a3c964f7c6baea70
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-05-03 18:57:23 +0900

    변신 버그 수정

--- Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Cue/WxCueNotify_Metamorphose.cpp
+++ Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Cue/WxCueNotify_Metamorphose.cpp
@@ -32,6 +32,18 @@ bool AWxCueNotify_Metamorphose::OnActive_Implementation(AActor* MyTarget, const 
 		return false;
 	}
 
+	// BP에서 추가한 외형 SkeletalMeshComponent가 있으면 그 컴포넌트의 메시를 교체한다.
+	// (AWxCharacterBase::ApplyEquipmentVisuals 와 동일 규칙)
+	for (USceneComponent* Child : MeshComp->GetAttachChildren())
+	{
+		if (USkeletalMeshComponent* SkelChild = Cast<USkeletalMeshComponent>(Child))
+		{
+			MeshComp = SkelChild;
+			break;
+		}
+	}
+
+	AffectedMeshComp = MeshComp;
 	OriginalMesh = MeshComp->GetSkeletalMeshAsset();
 	MeshComp->SetSkeletalMeshAsset(MetamorphoseMesh);
 
@@ -42,19 +54,12 @@ bool AWxCueNotify_Metamorphose::OnRemove_Implementation(AActor* MyTarget, const 
 {
 	Super::OnRemove_Implementation(MyTarget, Parameters);
 
-	ACharacter* TargetCharacter = Cast<ACharacter>(MyTarget);
-	if (!TargetCharacter)
+	if (USkeletalMeshComponent* MeshComp = AffectedMeshComp.Get())
 	{
-		return true;
+		MeshComp->SetSkeletalMeshAsset(OriginalMesh);
 	}
 
-	USkeletalMeshComponent* MeshComp = TargetCharacter->GetMesh();
-	if (!MeshComp)
-	{
-		return true;
-	}
-
-	MeshComp->SetSkeletalMeshAsset(OriginalMesh);
+	AffectedMeshComp.Reset();
 	OriginalMesh = nullptr;
 
 	return true;
--- Plugins/WxCombat/Source/WxCombat/Public/AbilitySystem/Cue/WxCueNotify_Metamorphose.h
+++ Plugins/WxCombat/Source/WxCombat/Public/AbilitySystem/Cue/WxCueNotify_Metamorphose.h
@@ -7,12 +7,16 @@
 #include "WxCueNotify_Metamorphose.generated.h"
 
 class USkeletalMesh;
+class USkeletalMeshComponent;
 
 /**
  * 변신 GameplayCue.
  *
  * GameplayEffect의 유효 기간 동안 캐릭터의 SkeletalMesh를 교체하고,
  * 이펙트가 만료되거나 제거되면 원래 메쉬로 복원한다.
+ *
+ * BP에서 캐릭터 메시에 추가 부착한 외형 SkeletalMeshComponent가 있으면 그 컴포넌트를 우선 대상으로 한다.
+ * (AWxCharacterBase::ApplyEquipmentVisuals 와 동일 규칙)
  */
 UCLASS(Abstract, Blueprintable)
 class WXCOMBAT_API AWxCueNotify_Metamorphose : public AGameplayCueNotify_Actor
@@ -33,4 +37,7 @@ protected:
 private:
 	UPROPERTY(Transient)
 	TObjectPtr<USkeletalMesh> OriginalMesh;
+
+	UPROPERTY(Transient)
+	TWeakObjectPtr<USkeletalMeshComponent> AffectedMeshComp;
 };
