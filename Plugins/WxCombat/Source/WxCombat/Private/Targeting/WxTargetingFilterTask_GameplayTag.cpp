d283a97c087417be88d6a48af62b09db11e2f4a7
Woogle <woojoo4050@gmail.com>
2026-07-18 02:25:15 +0900

    게임플레이 타겟팅 필터에 태그 조건 주가

---
+++ Plugins/WxCombat/Source/WxCombat/Private/Targeting/WxTargetingFilterTask_GameplayTag.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Woogle. All Rights Reserved.
+
+
+#include "Targeting/WxTargetingFilterTask_GameplayTag.h"
+
+#include "GameplayTagAssetInterface.h"
+
+bool UWxTargetingFilterTask_GameplayTag::ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const
+{
+	const AActor* TargetActor = TargetData.HitResult.GetActor();
+	if (!TargetActor)
+	{
+		return false;
+	}
+	
+	const IGameplayTagAssetInterface* TagInterface = Cast<IGameplayTagAssetInterface>(TargetActor);
+	if (!TagInterface)
+	{
+		return false;
+	}
+	
+	FGameplayTagContainer OwnedTags;
+	TagInterface->GetOwnedGameplayTags(OwnedTags);
+	
+	return TagRequirements.RequirementsMet(OwnedTags);
+}
---
+++ Plugins/WxCombat/Source/WxCombat/Public/Targeting/WxTargetingFilterTask_GameplayTag.h
@@ -0,0 +1,23 @@
+﻿// Copyright Woogle. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "Tasks/TargetingFilterTask_BasicFilterTemplate.h"
+#include "WxTargetingFilterTask_GameplayTag.generated.h"
+
+/**
+ * 대상이 보유한 태그 조건으로 필터링.
+ */
+UCLASS()
+class WXCOMBAT_API UWxTargetingFilterTask_GameplayTag : public UTargetingFilterTask_BasicFilterTemplate
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual bool ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const override;
+	
+	UPROPERTY(EditAnywhere)
+	FGameplayTagRequirements TagRequirements;
+};
