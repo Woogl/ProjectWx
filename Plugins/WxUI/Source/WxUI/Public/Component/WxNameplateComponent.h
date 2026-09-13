09041a2021669bc5ab46e25a6bf4a464e1801b8a
Woogle <woojoo4050@gmail.com>
2026-09-13 20:18:30 +0900

    네임플레이트를 MVVM 리졸버 기반으로 단순화

--- Plugins/WxUI/Source/WxUI/Public/Component/WxNameplateComponent.h
+++ Plugins/WxUI/Source/WxUI/Public/Component/WxNameplateComponent.h
@@ -7,10 +7,8 @@
 #include "WxNameplateComponent.generated.h"
 
 class APawn;
-class UMVVMView;
-class UWxViewModel_Character;
 
-/** 캐릭터 VM을 주입하고 거리 기반 표시와 스케일을 적용한다. 태그 가시성은 WBP 바인딩이 담당한다. */
+/** 거리 기반 표시와 스케일만 담당한다. 캐릭터 데이터와 태그 가시성은 WBP의 MVVM이 처리한다. */
 UCLASS(ClassGroup = (Wx), meta = (BlueprintSpawnableComponent))
 class WXUI_API UWxNameplateComponent : public UWidgetComponent
 {
@@ -19,13 +17,9 @@ class WXUI_API UWxNameplateComponent : public UWidgetComponent
 public:
 	UWxNameplateComponent();
 
-	virtual void InitWidget() override;
-	virtual void SetWidget(UUserWidget* InWidget) override;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-
 	UPROPERTY(EditAnywhere, Category = "Wx", meta = (ClampMin = "0", Units = "cm"))
 	float ReferenceDistance = 1000.f;
 
@@ -40,14 +34,7 @@ protected:
 	float MaxVisibilityDistance = 3000.f;
 
 private:
-	void BindViewModel();
-	void ReleaseViewModel();
+	friend class FWxNameplatePresentationTest;
+
 	void UpdatePresentation(const APawn* ViewerPawn);
-
-	UPROPERTY(Transient)
-	TObjectPtr<UWxViewModel_Character> CharacterViewModel;
-
-	TWeakObjectPtr<UMVVMView> BoundView;
-	FName BoundSourceName;
-	bool bBindingErrorReported = false;
 };
