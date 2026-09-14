602d4efa36e266671f437628f2958d2bcbbe7b6c
Woogle <woojoo4050@gmail.com>
2026-09-14 23:51:37 +0900

    무적 효과에 직접 피해 GE 면역을 추가

--- Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Effect/WxEffect_Invincible.cpp
+++ Plugins/WxCombat/Source/WxCombat/Private/AbilitySystem/Effect/WxEffect_Invincible.cpp
@@ -1,14 +1,23 @@
 // Copyright Woogle. All Rights Reserved.
 
 #include "AbilitySystem/Effect/WxEffect_Invincible.h"
+#include "AbilitySystem/Effect/WxEffect_Damage.h"
 #include "AbilitySystemComponent.h"
 #include "GameplayEffectComponents/AssetTagsGameplayEffectComponent.h"
+#include "GameplayEffectComponents/ImmunityGameplayEffectComponent.h"
 #include "GameplayEffectComponents/TargetTagsGameplayEffectComponent.h"
 #include "WxGameplayTags.h"
 
 UWxEffect_Invincible::UWxEffect_Invincible()
 {
 	DurationPolicy = EGameplayEffectDurationType::Infinite;
+
+	// Hit의 회피 판정을 거치지 않는 직접 피해 적용도 차단한다.
+	UImmunityGameplayEffectComponent* ImmunityComp = CreateDefaultSubobject<UImmunityGameplayEffectComponent>(TEXT("Immunity"));
+	FGameplayEffectQuery DamageQuery;
+	DamageQuery.EffectDefinition = UWxEffect_Damage::StaticClass();
+	ImmunityComp->ImmunityQueries.Add(DamageQuery);
+	GEComponents.Add(ImmunityComp);
 
 	UTargetTagsGameplayEffectComponent* TargetTagsComp = CreateDefaultSubobject<UTargetTagsGameplayEffectComponent>(TEXT("TargetTags"));
 	FInheritedTagContainer GrantedTags;
